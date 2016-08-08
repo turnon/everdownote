f0af59232e280154fe68028bc2f033a5e9cfde52
ken <block24block@gmail.com>
2016-08-08 20:50:10 +0800

    test get notebook by name

--- test/note_store_test.rb
+++ test/note_store_test.rb
@@ -12,4 +12,16 @@ class TestNoteStore < MiniTest::Unit::TestCase
   def test_get_note_store
     assert_kind_of NoteStore, note_store
   end
+
+  def test_get_notebook_by_name
+    prebuilt = note_store.listNotebooks do |b|
+      b.name == '示例笔记本'
+    end
+    assert_equal 1, prebuilt.size
+
+    metas = note_store.findNotes({notebookGuid: prebuilt[0].guid}).notes
+    titles = metas.map(&:title)
+    assert_includes titles, '保存照片'
+  end
+
 end

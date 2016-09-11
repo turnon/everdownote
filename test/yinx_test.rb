ef987d65edcf6d6106cf7287481849a8ed6acacc
ken <block24block@gmail.com>
2016-09-11 12:11:16 +0800

    filter by books

--- test/yinx_test.rb
+++ test/yinx_test.rb
@@ -10,12 +10,20 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_match(/2016/, info)
   end
 
-  def test_take_block_to_filter_notes
+  def test_take_block_to_filter_notes_by_book_name
     notes = Yinx.new do
       book :book_1
     end
     assert_equal 2, notes.size
     assert_equal 1, notes.count{|n| n.title == 'note_1'}
+  end
+
+  def test_take_block_to_filter_notes_by_book_names
+    notes = Yinx.new do
+      book :book_1, /^book_/
+    end
+    assert_equal 3, notes.size
+    assert_equal 2, notes.count{|n| n.title == 'note_1'}
   end
 
 end

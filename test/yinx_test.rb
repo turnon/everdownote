962375c29b602337de16d847a5a6131947fcac0a
ken <block24block@gmail.com>
2016-09-17 11:12:55 +0800

    simplify test

--- test/yinx_test.rb
+++ test/yinx_test.rb
@@ -3,14 +3,7 @@ require 'yinx'
 
 class TestYinx < MiniTest::Unit::TestCase
 
-  def test_show_all_but_breif
-    skip
-    info = Yinx.new
-    assert_match %r(stack_1/book_1/note_1), info
-    assert_match(/2016/, info)
-  end
-
-  def test_take_block_to_filter_notes_by_book_name
+  def test_filter_by_book_name
     notes = Yinx.new do
       book :book_1
     end
@@ -18,7 +11,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal 1, notes.count{|n| n.title == 'note_1'}
   end
 
-  def test_take_block_to_filter_notes_by_book_names
+  def test_filter_by_book_names
     notes = Yinx.new do
       book :book_1, /^book_/
     end
@@ -26,7 +19,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal 2, notes.count{|n| n.title == 'note_1'}
   end
 
-  def test_take_block_to_filter_notes_by_stacks
+  def test_filter_by_stacks
     notes = Yinx.new do
       stack :stack_1, :stack_2
     end
@@ -34,7 +27,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal 2, notes.count{|n| n.title == 'note_1'}
   end
 
-  def test_take_block_to_filter_notes_by_tags
+  def test_filter_by_tags
     notes = Yinx.new do
       tag :tag_1
     end
@@ -42,7 +35,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal %w{note_3}, notes.map{|n| n.title}
   end
 
-  def test_take_block_to_filter_notes_by_word_in_one_note
+  def test_filter_by_word_in_one_note
     notes = Yinx.new do
       word 'in_note3'
     end
@@ -50,7 +43,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal %w{note_3}, notes.map{|n| n.title}
   end
 
-  def test_take_block_to_filter_notes_by_word_in_more_than_one_note
+  def test_filter_by_word_in_more_than_one_note
     notes = Yinx.new do
       word 'qwertyuiop'
     end
@@ -58,7 +51,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal %w{note_2 note_3}, notes.map{|n| n.title}
   end
 
-  def test_take_block_to_filter_notes_by_words
+  def test_filter_by_words
     notes = Yinx.new do
       word 'qwertyuiop in_note3'
     end
@@ -66,7 +59,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal %w{note_3}, notes.map{|n| n.title}
   end
 
-  def test_take_block_to_filter_notes_by_words_in_array
+  def test_filter_by_words_in_array
     notes = Yinx.new do
       word %w{qwertyuiop in_note3}
     end
@@ -74,7 +67,7 @@ class TestYinx < MiniTest::Unit::TestCase
     assert_equal %w{note_3}, notes.map{|n| n.title}
   end
 
-  def test_take_block_to_filter_notes_by_symbol_word
+  def test_filter_by_symbol_word
     notes = Yinx.new do
       word :in_note3
     end
